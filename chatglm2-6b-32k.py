4c2ec8bf50fd9f375940d12039fe4a1375f309e7
Bryne Lee <bryne_lxd@sina.com>
2023-10-04 21:32:13 +0800

    added chatglm2-6b-32k.py and qwenstudy.py

--- chatglm2-6b-32k.py
+++ chatglm2-6b-32k.py
@@ -1,9 +1,11 @@
 
 from transformers import AutoTokenizer, AutoModel
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True).half().cuda()
+# tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True).half().cuda()
+tokenizer = AutoTokenizer.from_pretrained("D:\\AI_Spaces\\models\\ChatGLM2-6B-32K", trust_remote_code=True)
+model = AutoModel.from_pretrained("D:\\AI_Spaces\\models\\ChatGLM2-6B-32K", trust_remote_code=True).half().cuda()
 model = model.eval()
 response, history = model.chat(tokenizer, "你好", history=[])
 print(response)
-response, history = model.chat(tokenizer, "晚上睡不着应该怎么办", history=history)
+response, history = model.chat(tokenizer, "请给出快速排序算法的python示例", history=history)
 print(response)
