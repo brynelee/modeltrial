5e5bc11b36bb6d560bfaccf178d0e3740b4ea6f1
Bryne Lee <bryne_lxd@sina.com>
2023-11-29 23:40:40 +0800

    added retriever demo

--- LangChainStudy/embedding_test.py
+++ LangChainStudy/embedding_test.py
@@ -1,6 +1,10 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 
+# embedding_model = HuggingFaceEmbeddings(model_name = "moka-ai/m3e-base")
+
 # embedding_model = HuggingFaceEmbeddings(model_name = "moka-ai/m3e-large")
+
+
 
 embedding_model = HuggingFaceEmbeddings(model_name = "shibing624/text2vec-base-chinese")
 
