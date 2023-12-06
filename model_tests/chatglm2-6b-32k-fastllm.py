545bebdff2fc2bf91ac267dd5450a36ac770e88d
Bryne Lee <bryne_lxd@sina.com>
2023-12-07 00:49:02 +0800

    added trial for fastllm

--- model_tests/chatglm2-6b-32k-fastllm.py
+++ model_tests/chatglm2-6b-32k-fastllm.py
@@ -3,8 +3,15 @@ from transformers import AutoTokenizer, AutoModel
 # tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True)
 # model = AutoModel.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True).half().cuda()
 tokenizer = AutoTokenizer.from_pretrained("/mnt/d/AI_Spaces/models/chatglm3-6b-32k", trust_remote_code=True)
-model = AutoModel.from_pretrained("/mnt/d/AI_Spaces/models/chatglm3-6b-32k", trust_remote_code=True).half().cuda()
-model = model.eval()
+model = AutoModel.from_pretrained("/mnt/d/AI_Spaces/models/chatglm3-6b-32k", trust_remote_code=True)
+
+# 加入下面这两行，将huggingface模型转换成fastllm模型
+# 目前from_hf接口只能接受原始模型，或者ChatGLM的int4, int8量化模型，暂时不能转换其它量化模型
+from fastllm_pytools import llm
+model = llm.from_hf(model, tokenizer, dtype = "float16") # dtype支持 "float16", "int8", "int4"
+
+# model = model.eval()
+
 response, history = model.chat(tokenizer, "你好", history=[])
 print(response)
 response, history = model.chat(tokenizer, "请给出快速排序算法的python示例", history=history)
